0b12773a8aaa51d96728c93cfb6b612a367b09d3
theprogmatheus <theprog.matheus@gmail.com>
2022-05-30 16:08:56 -0300

    Criando componentes do projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+// CSS
 import './App.css';
+
+// React
+import { useCallback, useEffect, useState } from 'react';
+
+// Data
+import { wordsList } from './data/words';
+
+// Components
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" }
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+  console.log(words);
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
   );
 }
