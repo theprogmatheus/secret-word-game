4b23468fd2278197513ea1dce85993cbba6aba42
theprogmatheus <theprog.matheus@gmail.com>
2022-05-30 15:26:34 -0300

    Criando o projeto

---
+++ src/data/words.js
@@ -0,0 +1,10 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+  profissão: ["Advogado", "Engenheiro", "Médico", "Ator", "Desenvolvedor"],
+  estado: ["goiás", "rio", "recife", "amapá", "paraná", "amazonas"]
+};
