55de02ef58009860a3e3279c68f1f390d0b979ea
theprogmatheus <theprog.matheus@gmail.com>
2022-05-30 16:25:24 -0300

    Alterando os estágios do jogo

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,11 +1,11 @@
 import './StartScreen.css';
 
-const StartScreen = () => {
+const StartScreen = ({ startGame }) => {
     return (
         <div className="start">
             <h1>Secret Word Game</h1>
             <p>Clique no botão abaixo para começar a jogar</p>
-            <button>Começar o jogo</button>
+            <button onClick={startGame}>Começar o jogo</button>
         </div>
     )
 }
